b8349bb18b0279e5b596341e8b86c842acafd93e
Johnson <erikjohnson@agcenter.lsu.edu>
2026-09-16 11:46:22 -0500

    This is just to show James how this works.

--- Scripts/00_VegSetup.R
+++ Scripts/00_VegSetup.R
@@ -2,6 +2,8 @@ setwd("~/Research/Urban Native Greens/Data/UNGVegData/")
 
 rm(list = ls()) # clear the Environment
 
+# new script
+
 # Load the data ----------------------------------------------------------------
 
 library(RODBC)
